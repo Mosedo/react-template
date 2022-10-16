90beaffba043d1d2ed10e08bc0d6edd12225ab3c
Musa Ibrahim <ibrahmmusauon@gmail.com>
2022-10-16 14:29:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React,{useState,useRef,useEffect} from 'react'
+import NavBarComponent from './components/NavBarComponent';
+
+function App() {
+  //https://jsonplaceholder.typicode.com/
+  
+
+  return (
+    <>
+      <NavBarComponent/>
+    </>
+  );
+}
+
+export default App;
